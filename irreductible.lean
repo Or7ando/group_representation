c8c1ad13e46723ac2f4e2ef3d5d29054ea209c5e
Or7ando <orlando_cau@gmx.fr>
2020-04-23 10:28:56 +0200

    Udapte of group representation

--- irreductible.lean
+++ irreductible.lean
@@ -1,6 +1,10 @@
+import tactic
 import .sub_module
+import tactic.push_neg
 import .morphism
+import .homothetic
 import .kernel
+open Kernel range morphism homothetic
 open stability
 universe variables u v w w'
 
@@ -9,20 +13,16 @@ lemma Fact {R : Type v}[ring R]{M1 : Type w}[add_comm_group M1] [module R M1]
     rcases hyp, assumption, rw hyp at hyp', rw submodule.mem_bot at hyp', by contradiction, 
 end
 
-
-variables {G : Type u} [group G] {R : Type v}[ring R] 
-
 namespace Irreductible
+variables {G : Type u} [group G] {R : Type v}[ring R] 
 variables  {M : Type w}[add_comm_group M] [module R M]  ( ρ : group_representation G R M) 
-
-
 def Irreductible :=  ∀ {p : submodule R M},   (stable_submodule ρ p) → (p = ⊤ ∨  p = ⊥)
 
 end Irreductible 
 
 namespace morphism.from_irreductible 
-open Irreductible
-open Kernel range 
+open Irreductible linear_map
+variables {G : Type u} [group G] {R : Type v}[ring R] 
 variables {M1 : Type w}  [add_comm_group M1] [module R M1]
           {M2 : Type w'} [add_comm_group M2] [module R M2] 
           { ρ1 : group_representation G R M1}
@@ -33,33 +33,99 @@ variables {M1 : Type w}  [add_comm_group M1] [module R M1]
 --  Faire les choses plus proprement en definissant une strucure ⊤ ⊥  sur les representation 
 --
 theorem pre_Schur_ker  (hyp : Irreductible ρ1) :  
-(linear_map.ker (f.ℓ  : M1 →ₗ[R] M2) = ⊤  ∨ linear_map.ker (f.ℓ : M1 →ₗ[R] M2)  = ⊥  ) := 
- hyp  (ker_is_stable_submodule f)
+(ker f.ℓ  = ⊤  ∨ ker f.ℓ   = ⊥  ) :=   hyp  (ker_is_stable_submodule f)
 
 
 theorem pre_Schur_range (hyp : Irreductible ρ2) :
- (linear_map.range (f.ℓ  : M1 →ₗ[R] M2) = ⊤  ∨ linear_map.range (f.ℓ  : M1 →ₗ[R] M2)  = ⊥  ) := 
+ (range f.ℓ  = ⊤  ∨ range f.ℓ   = ⊥  ) := 
  hyp (range_is_stable_submodule f)
+ open linear_map
+variables (hyp : Irreductible ρ2 )
+#check f
+#check  pre_Schur_range f (by assumption) 
 /--
     little modification of Serre `Representations linéaires des groupes  finis`. 
     We have no assumption of field just a ring (`NOT` only commutative). `NOT` dimension. 
 -/
-theorem Schur₁  (hyp1 : Irreductible ρ1)(hyp2 : Irreductible ρ2) : (∃ x : M1, (f.ℓ  : M1 →ₗ[R] M2)  x ≠ 0) →  
-(linear_map.ker (f.ℓ : M1 →ₗ[R] M2) = ⊥ ) ∧  linear_map.range (f.ℓ  : M1→ₗ[R] M2) = ⊤ := begin 
-intros hyp_not_nul, rcases hyp_not_nul with ⟨x,hyp_not_nul⟩,
-split, swap, apply Fact, 
-    apply pre_Schur_range,assumption, 
+
+
+
+
+
+theorem Schur₁  (hyp1 : Irreductible ρ1)(hyp2 : Irreductible ρ2) : (∃ x : M1, (f.ℓ  x ≠ 0)) →  
+(ker f.ℓ  = ⊥ ) ∧  range f.ℓ  = ⊤ := 
+begin
+    intros hyp_not_nul,
+    rcases hyp_not_nul with ⟨x,hyp_not_nul⟩,
+    split, swap, 
+    apply Fact, -- ici ?  
+    apply pre_Schur_range, --- on ne comprend plus !
+    assumption, 
     rw linear_map.mem_range,
     use x,
     exact hyp_not_nul,
     let schur :=  (hyp1 (ker_is_stable_submodule f)),unfold Ker at schur,
     rcases schur, swap,assumption,
     rw schur,
-    have R :  x ∈ linear_map.ker (f.ℓ  : M1 →ₗ[R] M2),
+    have R :  x ∈ ker f.ℓ ,
             rw schur, trivial,
     rw linear_map.mem_ker at R,
     trivial,
 end
-
-#lint
 end morphism.from_irreductible
+namespace Schur₂
+open Irreductible morphism.from_irreductible
+open_locale classical
+variables {G : Type u} [group G] {R : Type v}[comm_ring R]{M : Type w}[add_comm_group M] [module R M]
+variables  {ρ : group_representation G R M}
+theorem Schur₂(f : ρ ⟶ ρ) [re : Irreductible ρ] : (∃ r : R, ∃ m0 : M, m0 ≠ 0 ∧  f.ℓ m0 + r • m0 = 0) → (∃ r : R, ∀ m : M, f.ℓ m + r • m = 0) := begin 
+    intro hyp,
+    rcases hyp with ⟨r,m0,⟨spec,spectral⟩⟩,
+    use r,
+    let g :=  f + r • 𝟙 ρ,
+    have  certif_m0_in_ker : g.ℓ  m0 = 0,
+        erw [add_ext,
+         smul_ext, one_ext],
+        exact spectral,
+    let schur := (Schur₁ g) ( by assumption  ) (by assumption), -- swap, exact re   
+    by_contra,                  -- re doesnt work 
+    push_neg at a,
+    rcases a with ⟨ζ,n ⟩, 
+    have R : (f.ℓ) ζ + r • ζ = g.ℓ ζ, 
+        erw  [add_ext,smul_ext,one_ext],
+        exact rfl, 
+    rw R at n, 
+    let V := (schur ⟨ζ,n⟩).1,
+    have : m0 ∈ linear_map.ker g.ℓ,
+        rw linear_map.mem_ker,
+        exact certif_m0_in_ker,
+    rw V at this,
+    rw submodule.mem_bot at this,
+    by contradiction,
+end
+/-
+example (P : Prop) (h : P ∨ ¬ P) : ¬ ¬ P → P := begin 
+    intros hnn,
+    cases h,
+    assumption,
+    by contradiction,
+end
+example (P Q : Prop) : (P → Q) ↔ (¬ Q → ¬ P) := begin 
+        split,
+        intro hpq,
+        intro hnQ,
+        intro hp,
+        end
+
+example (X :Type) (P : X → Prop ) (hyp : (∃ x : X, ¬ (P x)) → false ) : ∀ x, P x := begin
+    intro,
+    contrapose! hyp,
+    use x,
+end
+
+example (X :Type) (P : X → Prop ) (hyp : (∃ x : X, ¬ (P x)) → false ) : ∀ x, P x := begin
+  by_contra,
+  rw not_forall at a,
+  exact hyp a,
+end
+-/
